703576df9685b86137d5e15755f1380d4b3c2c93
Denys_Shestopal <deniskus@bigmir.net>
2016-10-06 18:51:43 +0300

    AppRunner modified (messages about water/food need added)

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -6,6 +6,10 @@ public class AppRunner {
         ccs.setCaloriesExpectation(1600);
         ccs.setWaterExpectation(2500);
         System.out.println("Daily calories result is: " + ccs.countDailyCaloriesResult());
+        if(ccs.countDailyCaloriesResult()>0)
+            System.out.println("Please eat " + ccs.countDailyCaloriesResult() + " calories more");
         System.out.println("Daily water result is: " + ccs.countDailyWaterResult());
+        if(ccs.countDailyWaterResult()>0)
+            System.out.println("Please drink " + ccs.countDailyWaterResult()+ "mls of water more.");
     }
 }
