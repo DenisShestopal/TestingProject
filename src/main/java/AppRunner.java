47a530c50c7122c1394fdd4442987477cb8b5eca
Denys_Shestopal <deniskus@bigmir.net>
2016-10-06 18:30:41 +0300

    go() to AppRunner added

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -1,6 +1,10 @@
 public class AppRunner {
 
     public void go() {
+        Ccs ccs = new Ccs();
+        ccs.setCalories(0);
+        ccs.setCaloriesExpectation(1600);
+        ccs.setWaterExpectation(2500);
 
     }
 }
